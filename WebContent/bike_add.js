ce84634e3e56b1df5ae6bc550e83f030f1485d30
Adam Bednarski <abednarski79@gmail.com>
2014-11-04 23:53:54 +0000

    bootstrap commit

--- WebContent/bike_add.js
+++ WebContent/bike_add.js
@@ -1,22 +1,18 @@
 $(document).ready(function() {
 	var userId = getURLParameter('userId');
-	$( "#button" ).bind( "click", function() {
-		addBike(userId);
+	$("#button").attr("href", 'bike_list.html?userId=' + userId);
+	$('#button').on('click', function(e) {
+		addBike(userId);    
 	});
 });
 
 function addBike(userId) {
-	var descriptionValue = $('#description').val();
-	console.log(descriptionValue);	
-	var uuIdValue = $('#uuId').val();
-	console.log(uuIdValue);
+	var descriptionValue = $('#description').val();	
+	var uuIdValue = $('#tagId').val();
 	var majorIdValue = $('#majorId').val();
-	console.log(majorIdValue);
 	var minorIdValue = $('#minorId').val();
-	console.log(minorIdValue);
 	var urlValue = 'http://api.dev.rothar.appbucket.eu/v2/users/' + userId + '/assets';		
-	var dataValue = {"uuid":uuIdValue, "minor":minorIdValue, "major":majorIdValue, "description":descriptionValue};
-	console.log(dataValue);
+	var dataValue = {"uuid":uuIdValue, "minor":minorIdValue, "major":majorIdValue, "description":descriptionValue};	
 	$.ajax({
 		type: "POST",
 		url: urlValue,
@@ -26,19 +22,9 @@ function addBike(userId) {
 		contentType: "application/json; charset=UTF-8"
 		})
 		.done(function( msg ) {
-		alert( "Data Saved: " + msg );
-		});
+			alert( "Data Saved");
+		})
+		 .fail(function() {
+			 alert( "error" );
+		 });
 }
-
-function getURLParameter(sParam) {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
-    }
-};
--- WebContent/bike_list.js
+++ WebContent/bike_list.js
@@ -1,5 +1,12 @@
 $(document).ready(
 		function() {
+			
+			$('#table').hide();			
+			$('#no_bikes').hide();
+			$('#loading').show();
+			var userId = getURLParameter('userId');
+			$("#button-add").attr("href", 'bike_add.html?userId=' + userId);
+			
 			var userId = getURLParameter('userId');
 			$.getJSON('http://api.dev.rothar.appbucket.eu/v2/users/'
 					+ userId + '/assets', function(bikes) {
@@ -8,18 +15,18 @@ $(document).ready(
 							'<tr>'
 							+ '<td>' + bike.uuid + '</td>' 
 							+ '<td>' + bike.description + '</td>' 
-							+ '<td><a class="btn btn-default" href="report_list.html?assetId=' + bike.assetId + '">Reports</a></td>'
-							+ '<td><a class="btn btn-default" href="bike_edit.html?assetId=' + bike.assetId + '">Edit</a></td>'
+							+ '<td><a class="btn btn-default" id="button-reports" href="report_list.html?assetId=' + bike.assetId + '">Reports</a></td>'
+							+ '<td><a class="btn btn-default" id="button-edit" href="bike_edit.html?assetId=' + bike.assetId + '">Edit</a></td>'
 							+ '</tr>');
 				});
 			}) .done(function(bikes) {
-				$('#loading').remove();
+				$('#loading').hide();			
 				if(bikes.length === 0) {
-					$('table tr:last').after(
-							'<tr>'
-							+ '<td colspan="4" class="text-center"> You have no registered bikes</td>' 							
-							+ '</tr>');
-				}				
+					$('#table').hide();
+					$('#no_bikes').show();
+				} else {
+					$('#table').show();
+					$('#no_bikes').hide();
+				}
 			});
-
 		});
