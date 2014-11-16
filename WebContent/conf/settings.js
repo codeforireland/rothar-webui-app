2f17d5a2a22143d93a88de011d9a9f764e72b020
Adam Bednarski <abednarski79@gmail.com>
2014-11-16 22:13:28 +0000

    conf update

--- WebContent/conf/settings.js
+++ WebContent/conf/settings.js
@@ -1,2 +1,2 @@
-/*var BACKEND_URL = "http://api.dev.rothar.appbucket.eu";*/
-var BACKEND_URL = "http://localhost:8080/Rothar";
+var BACKEND_URL = "http://api.dev.rothar.appbucket.eu";
+/*var BACKEND_URL = "http://localhost:8080/Rothar";*/
